37e83189515b60e2be87d06e9a87bbd1fc1a1180
Evgeniy Grishko <evgeniygrish@gmail.com>
2022-08-01 23:17:03 +0300

    fix

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:balor1989.github.io/learning-vue.git main:gh-pages
+git push -f git@github.com:Balor1989/learning-vue.git master:gh-pages
 cd -
