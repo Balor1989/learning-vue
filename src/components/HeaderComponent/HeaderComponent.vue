ebd44310d68b6031c907eb29ca9dcac2398881a5
Evgeniy Grishko <evgeniygrish@gmail.com>
2022-08-01 11:01:44 +0300

    adds style

--- src/components/HeaderComponent/HeaderComponent.vue
+++ src/components/HeaderComponent/HeaderComponent.vue
@@ -1,3 +1,25 @@
 <template>
-  <h2>HeaderComponent</h2>
+  <div class="card center">
+    <h2 class="header-title">{{ title }}</h2>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum doloremque
+      hic consequatur voluptate? Dolore, iure!
+    </p>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "This is the header from data",
+    };
+  },
+};
+</script>
+
+<style>
+.header-title {
+  font-weight: 700;
+}
+</style>
